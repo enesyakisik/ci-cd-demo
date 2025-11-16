61a91f5bad6b2249c45bb9ed428770f2a82a1c46
Enes <enes@192.168.1.12>
2025-11-16 18:39:17 +0300

    test commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Pipeline is working successfully!"})
+    return jsonify({"message": "Pipeline is working successfully! Enes Yakışık "})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+ß
